2addc08803aa5794f11f292879072f1aef288278
DranoelMit <tim.matthew.leonard@gmail.com>
2019-05-29 10:36:48 -0400

    Add all routes to navBar

--- psk-website/src/App.js
+++ psk-website/src/App.js
@@ -3,6 +3,8 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './pages/home';
 import Brothers from './pages/brothers';
 import Alumni from './pages/alumni';
+import Recruitment from './pages/recruitment';
+import Contact from './pages/contact';
 import NavBar from './components/navBar';
 import { Container } from 'semantic-ui-react'
 import './App.css';
@@ -17,6 +19,8 @@ class App extends Component {
             <Route path='/home' component={Home} />
             <Route path='/brothers' component={Brothers} />
             <Route path='/alumni' component={Alumni} />
+	    <Route path='/recruitment' component={Recruitment} />
+	    <Route path='/contact' component={Contact} />
             <Redirect to='/home' />
           </Switch>
         </Container>
